eee45ee3dfb92bbc8e27cb5614cdae94956d3f42
barba_laptop <joel.barba.vidal@gmail.com>
2016-03-04 18:08:45 +0100

    Alpha 1 (without detail)

--- client/main.js
+++ client/main.js
@@ -14,58 +14,207 @@ Router.route('/principal', function () {
   this.render('principal_screen', { to : "main" });
 });
 
+
+/// Accounts config
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+
+/// Infinite scroll config
+Session.set("itemsLimit", 40);
+
+
+
+
 /////
 // template helpers 
 /////
 
+Template.body.helpers({
+  username:function(){
+    if (Meteor.user()) {
+      return Meteor.user().username;
+    } else {
+      return "anonymous internet user";
+    }
+  }
+});
+
 // helper function that returns all available websites
 Template.website_list.helpers({
     websites:function(){
-        return Websites.find({});
+        return Websites.find(
+            {},
+            { sort  : { count_vup: -1, count_vdw: 1 }, 
+              limit : Session.get("itemsLimit")}
+        );
+        
+//        return Websites.collection.aggregate([
+//            { $unwind: "votes_up"}, 
+//            { $group: {_id:"$_id", votes_up: {$push:"votes_up"}, size: {$sum:1}}}, 
+//            { $sort:{size:1}}
+//        ]);
     }
 });
+
+
+
+// helper functions for each item in the list
+Template.website_item.helpers({
+    
+    prettifyDate: function(ts) {
+        var month = ts.getMonth() + 1;
+        return ts.getDate() + '-' + month + '-' + ts.getFullYear()
+               + ' ' + ts.getHours() + ':' + ts.getMinutes() + ':' + ts.getSeconds();
+    },
+    
+    getUser: function(user_id){
+      var user = Meteor.users.findOne({_id:user_id});
+      if (user){
+        return user.username;
+      } else {
+        return 'nobody';
+      }
+    },
+
+    votedUp: function() {
+        return (test_user_vote(this._id, Meteor.user()._id) == 1);
+    },    
+    votedDown: function() {
+        return (test_user_vote(this._id, Meteor.user()._id) == -1);
+    }
+});
+
 
 
 /////
 // template events 
 /////
 
+// Return 1: if there's an up vote, 0: no vote, -1: down vote
+function test_user_vote(website_id, user_id) {
+    if (Websites.findOne( { _id: website_id, votes_up  : { $all: [user_id] }} ) ) { return 1; }
+    if (Websites.findOne( { _id: website_id, votes_down: { $all: [user_id] }} ) ) { return -1; }
+    return 0;
+}
+
+
 Template.website_item.events({
-    "click .js-upvote":function(event){
-        // example of how you can access the id for the website in the database
-        // (this is the data context for the template)
+    "click .js-upvote": function(event) {
         var website_id = this._id;
-        console.log("Up voting website with id "+website_id);
-        // put the code in here to add a vote to a website!
+        
+        if (Meteor.user()) {    // Only logged users can vote
 
-        return false;// prevent the button from reloading the page
+            var user_id = Meteor.user()._id;
+
+            // Test if the user exist in the vote array
+            var vote = test_user_vote(website_id, user_id);
+            console.log('The user voted before:' + vote + ', webid: ' + website_id);
+
+            // Change vote from down to up
+            if (vote == -1) Websites.update( {_id: website_id},  { $pull: { votes_down: user_id }, $inc: { count_vdw: -1 } } );
+
+            // Unvote
+            if (vote == 1)  Websites.update( {_id: website_id},  { $pull: { votes_up: user_id }, $inc: { count_vup: -1 } } );
+
+            if (vote != 1) {    // Inser vote only if it doesn't exist
+                Websites.update(
+                            {_id: website_id}, 
+                            { $push: { votes_up: user_id }, $inc: { count_vup: 1 } }
+                        );
+            }
+        } else {
+            $('#must_log_warning_' + website_id).show();
+        }
+        return false; // prevent the button from reloading the page
     }, 
-    "click .js-downvote":function(event){
-
-        // example of how you can access the id for the website in the database
-        // (this is the data context for the template)
+        
+    "click .js-downvote": function(event) {
         var website_id = this._id;
-        console.log("Down voting website with id "+website_id);
 
-        // put the code in here to remove a vote from a website!
+        if (Meteor.user()) {    // Only logged users can vote
+            
+            var user_id = Meteor.user()._id;
 
-        return false;// prevent the button from reloading the page
+            // Test if the user exist in the vote array
+            var vote = test_user_vote(website_id, user_id);
+            console.log('The user voted before:' + vote + ', webid: ' + website_id);
+
+            // Change vote from up to down
+            if (vote == 1) Websites.update( {_id: website_id},  { $pull: { votes_up: user_id }, $inc: { count_vup: -1 } } );
+
+            // Unvote
+            if (vote == -1)  Websites.update( {_id: website_id},  { $pull: { votes_down: user_id }, $inc: { count_vdw: -1 } } );
+
+            if (vote != -1) {   // Inser vote only if it doesn't exist
+                Websites.update(
+                            {_id: website_id}, 
+                            { $push: { votes_down: user_id }, $inc: { count_vdw: 1 } }
+                        );
+            }
+        } else {
+            $('#must_log_warning_' + website_id).show();
+        }
+        return false; // prevent the button from reloading the page
+    },
+    
+    "click .js-hide-alert": function(event) {
+        $('#must_log_warning_' + this._id).hide("fast");
     }
 })
 
-Template.website_form.events({
+
+/// Template related to the behaivor of the new website form
+Template.website_add_new.events({
     "click .js-toggle-website-form":function(event){
-        $("#website_form").toggle('slow');
+        $("#website_add_new").toggle('slow');
     }, 
     "submit .js-save-website-form":function(event){
 
-        // here is an example of how to get the url out of the form:
-        var url = event.target.url.value;
-        console.log("The url they entered is: "+url);
-
-        //  put your website saving code in here!	
+        if (Meteor.user()) {
+            var url = event.target.url.value;
+            var new_web = {
+                url         : event.target.url.value,
+                title       : event.target.title.value,
+                description : event.target.description.value,
+                createdOn   : new Date(),
+                createdBy   : Meteor.user()._id,
+                votes_up    : [],
+                votes_down  : [],
+                count_vup   : 0,
+                count_vdw   : 0
+            };
+//            console.log('url:' + new_web.url);
+//            console.log('title:' + new_web.title);
+//            console.log('description:' + new_web.description);
+            
+            // Websites posted by users should have an URL and a description.
+            if (!new_web.url || !new_web.description) {
+                $('#required_warning').show();
+                return false;
+            } else {
+                // Insert to collection
+                Websites.insert(new_web);
+                
+                // Clear form fields
+                event.target.url.value = '';
+                event.target.title.value = '';
+                event.target.description.value = '';
+                
+                // Close form
+                $("#website_add_new").toggle("slow", function() {
+                    // Animation complete.
+                });
+                return false;                
+            }
+        }        
 
         return false;// stop the form submit from reloading the page
 
     }
 });
+
+
+
+
--- server/startup.js
+++ server/startup.js
@@ -4,34 +4,59 @@ Meteor.startup(function () {
 if (!Websites.findOne()){
     console.log("No websites yet. Creating starter data.");
       Websites.insert({
-        title:"Goldsmiths Computing Department", 
-        url:"http://www.gold.ac.uk/computing/", 
-        description:"This is where this course was developed.", 
-        createdOn:new Date()
+        title       : "Goldsmiths Computing Department", 
+        url         : "http://www.gold.ac.uk/computing/", 
+        description : "This is where this course was developed.", 
+        createdOn   : new Date(),
+        createdBy   : 'nobody',
+        votes_up    : [],
+        votes_down  : [],
+        count_vup   : 0,
+        count_vdw   : 0
     });
      Websites.insert({
-        title:"University of London", 
-        url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
-        description:"University of London International Programme.", 
-        createdOn:new Date()
+        title       : "University of London", 
+        url         : "http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
+        description : "University of London International Programme.", 
+        createdOn   : new Date(),
+        createdBy   : 'nobody',
+        votes_up    : [],
+        votes_down  : [],
+        count_vup   : 0,
+        count_vdw   : 0
     });
      Websites.insert({
-        title:"Coursera", 
-        url:"http://www.coursera.org", 
-        description:"Universal access to the world’s best education.", 
-        createdOn:new Date()
+        title       : "Coursera", 
+        url         : "http://www.coursera.org", 
+        description : "Universal access to the world’s best education.", 
+        createdOn   : new Date(),
+        createdBy   : 'nobody',
+        votes_up    : [],
+        votes_down  : [],
+        count_vup   : 0,
+        count_vdw   : 0
     });
     Websites.insert({
-        title:"Google", 
-        url:"http://www.google.com", 
-        description:"Popular search engine.", 
-        createdOn:new Date()
+        title       : "Google", 
+        url         : "http://www.google.com", 
+        description : "Popular search engine.", 
+        createdOn   : new Date(),
+        createdBy   : 'nobody',
+        votes_up    : [],
+        votes_down  : [],
+        count_vup   : 0,
+        count_vdw   : 0
     });
     Websites.insert({
-        title:"El Blog del Barba", 
-        url:"https://blogdelbarba.wordpress.com/", 
-        description:"This is a very interesting blog page.", 
-        createdOn:new Date()
+        title       : "El Blog del Barba", 
+        url         : "https://blogdelbarba.wordpress.com/", 
+        description : "This is a very interesting blog page.", 
+        createdOn   : new Date(),
+        createdBy   : 'nobody',
+        votes_up    : [],
+        votes_down  : [],
+        count_vup   : 0,
+        count_vdw   : 0
     });
     
 }
